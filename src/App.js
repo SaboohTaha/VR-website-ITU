8599f958753ec01ea66123aa3e1fff92782f7a3f
Sabooh Taha <msabooh_taha@hotmail.com>
2023-10-03 17:16:27 +0500

    Add route for Navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/home';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/' element={<Navbar />} />
     </Routes>
   );
 }
